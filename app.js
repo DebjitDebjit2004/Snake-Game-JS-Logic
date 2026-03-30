12f64b9a7e390eda0646b593ffe42489ce2a83a1
DebjitDebjit2004 <dasdebjit1947@gmail.com>
2026-03-30 11:10:49 +0530

    Feat: Render Logic Snake - Snake is Accuretly Moving on Board

--- app.js
+++ app.js
@@ -1,14 +1,55 @@
 const board = document.querySelector('.board')
-const blockHeight = 30
-const blockWidth = 30
+const blockHeight = 60
+const blockWidth = 60
 
 const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
+
+const blocks = []
+const snake = [
+    {
+        x:1, y:6
+    }
+]
+
+let direction = 'down'
+
 
 for (let row = 0; row < rows; row++) {
     for (let col = 0; col < cols; col++) {
         const block = document.createElement('div');
         block.classList.add("block");
         board.appendChild(block);
+        block.innerText = `${row}-${col}`
+        blocks[`${row}-${col}`] = block
     }
 }
+
+function render() {
+    snake.forEach(segment => {
+        blocks[`${segment.x}-${segment.y}`].classList.add('fill')
+    })
+}
+
+setInterval(() => {
+    let head = null;
+
+    if (direction == "left") {
+        head = {x: snake[0].x, y: snake[0].y - 1}
+    } else if (direction == "right") {
+        head = {x: snake[0].x, y: snake[0].y + 1}
+    } else if (direction == "down") {
+        head = {x: snake[0].x + 1, y: snake[0].y}
+    } else if (direction == "up") {
+        head = {x: snake[0].x - 1, y: snake[0].y}
+    }
+
+    snake.forEach(segment => {
+        blocks[`${segment.x}-${segment.y}`].classList.remove('fill')
+    })
+
+    snake.unshift(head)
+    snake.pop()
+
+    render()
+}, 400)
